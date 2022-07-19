5aef4d7e31d99ed4747e057612fc67d775395929
月之领主LM <178910432@qq.com>
2022-07-20 04:04:19 +0800

    Update

--- init.bat
+++ init.bat
@@ -6,3 +6,7 @@ call npm --version
 call npm init
 call npm install -D "vuepress@next"
 call npm install -D "@vuepress/plugin-search@next"
+
+:: check this files
+:: \.github\workflows\node.js.yml
+:: \.github\workflows\vuepress-build-docs.yml
