a67e1f57e7ffe50258fe2b9ed2a2135c5eab2489
MoonLord-LM <178910432@qq.com>
2023-05-08 17:20:15 +0800

    Update

--- vuepress-starter/init.bat
+++ vuepress-starter/init.bat
@@ -1,19 +1,24 @@
 :begin
 
+
 call node --version
 call npm --version
-
 if not exist "package.json" ( call npm init )
 
-call npm list "vuepress@next" ^
- || call npm install -D "vuepress@next"
-call npm list "@vuepress/plugin-search@next" ^
- || call npm install -D "@vuepress/plugin-search@next"
-call npm list "vuepress-plugin-md-enhance@next" ^
- || call npm install -D "vuepress-plugin-md-enhance@next"
+
+call del /F /S /Q "package-lock.json" >nul 2>nul
+:: call rmdir /S /Q "node_modules" >nul 2>nul
+:: call npm cache clean --force
+
+
+call npm install -D "vuepress@next" && call npm list "vuepress@next"
+call npm install -D "@vuepress/plugin-search@next" && call npm list "@vuepress/plugin-search@next"
+call npm install -D "vuepress-plugin-md-enhance" && call npm list "vuepress-plugin-md-enhance"
+
 
 call npm run docs:build
 call npm run docs:dev
+
 
 pause
 cls
