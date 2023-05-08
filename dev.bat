2eddcbb52822f462466d3f666c0ef3e178f86e3f
MoonLord-LM <178910432@qq.com>
2023-05-08 16:34:50 +0800

    Update

--- dev.bat
+++ dev.bat
@@ -1,14 +1,5 @@
 :begin
 
-call npm list "vuepress@next" ^
-  || call npm install -D "vuepress@next"
-
-call npm list "@vuepress/plugin-search@next" ^
-  || call npm install -D "@vuepress/plugin-search@next"
-
-call npm list "vuepress-plugin-md-enhance@next" ^
-  || call npm install -D "vuepress-plugin-md-enhance@next"
-
 call npm run docs:dev
 
 pause
