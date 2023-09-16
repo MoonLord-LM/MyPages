2aba78e8a2eec3c02d0e44773141836f4731adfb
MoonLord-LM <178910432@qq.com>
2023-09-17 03:59:48 +0800

    Update start.bat

--- start/start.bat
+++ start/start.bat
@@ -2,6 +2,8 @@
 
 del /F /S /Q "package.json" >nul 2>nul
 del /F /S /Q "package-lock.json" >nul 2>nul
+rmdir /S /Q "node_modules"
+rmdir /S /Q "docs"
 
 echo {"name":"mypages","version":"1.0.0","description":"Static Site Generator Test","scripts":{"docs:dev":"vuepress dev docs","docs:build":"vuepress build docs"}}>"package.json"
 
@@ -9,6 +11,9 @@ call npm init -y
 call npm install -D "vuepress@next"
 call npm install -D "@vuepress/plugin-search@next"
 call npm install -D "vuepress-plugin-md-enhance"
+
+mkdir "docs"
+echo # Hello VuePress > "docs/README.md"
 
 call npm run docs:build
 call npm run docs:dev
